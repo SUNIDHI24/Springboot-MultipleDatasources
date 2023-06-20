de0d8060496b957927974575945e1b37755b6464
HAPPIESTMINDS\Kunte.keshava <kunte.keshava@happiestminds.com>
2023-06-20 13:49:02 +0530

    feature1 added

--- src/main/java/com/hm/multiple/datasource/book/controller/BookController.java
+++ src/main/java/com/hm/multiple/datasource/book/controller/BookController.java
@@ -2,7 +2,6 @@ package com.hm.multiple.datasource.book.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +36,7 @@ public class BookController {
 	public Book addBook(@RequestBody Book book) {
 		return bookService.addBook(book);
 	}
+	
 	//feature-1
 	@GetMapping("/getBook/{bookId}")
 	
